7d6d0563550dd557db5f9e74db3dc8904cc8a067
Jake Elsberry <jelsberry96@gmail.com>
2023-10-06 11:48:02 -0700

    establish client class

--- HairSalon/Models/Clients.cs
+++ HairSalon/Models/Clients.cs
@@ -2,6 +2,15 @@ namespace HairSalon.Models
 {
     public class Client 
     { 
+      public int ClientId { get; set; }
+      public string Name { get; set; }
+      public string HairType { get; set; }
+
+      // foreign key relating to Stylist table
+      // public int StylistId { get; set; }
+
+      // Reference Navigation Property - establish 1-many relationship
+      // public Stylist Stylist { get; set; }
 
     }
 }
