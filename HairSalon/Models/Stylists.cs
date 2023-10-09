43c1bb1640a76cc9715538e8b5ae7e279dff81f6
Jake Elsberry <jelsberry96@gmail.com>
2023-10-08 17:19:30 -0700

    follow project naming conventions for Sql

--- HairSalon/Models/Stylists.cs
+++ HairSalon/Models/Stylists.cs
@@ -5,6 +5,8 @@ namespace HairSalon.Models
     public class Stylist
     {
         public string Name { get; set; }
+        public int HoursPerWeek { get; set; }
+        
         public int StylistId { get; set; }
 
         public List<Client> Clients { get; set; }
